c90a4c2831b99a18c71586c7de2f3eb7aa3bf66d
Simon Norris <sumanneupane19982055@gmail.com>
2024-08-22 11:08:08 +0545

    setup db

---
+++ src/main/java/com/learn/spring_security/userManagement/entity/User.java
@@ -0,0 +1,26 @@
+package com.learn.spring_security.userManagement.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    private String firstname;
+    private String lastname;
+
+    private String password;
+}
